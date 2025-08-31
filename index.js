7ad68f6a001c6e2468d99095b31859a62a167286
kotteeswaran2005 <kotteeswaran2005@gmail.com>
2025-08-31 22:32:48 +0530

    almost done

--- index.js
+++ index.js
@@ -9,5 +9,15 @@ const observe = new IntersectionObserver((entries)=>{
     })
 } , {})
 
-const element = document.querySelectorAll("#section")
+// const observe = new IntersectionObserver((entries, observer) => {
+//   entries.forEach(entry => {
+//     if (entry.isIntersecting) {
+//       loadNewElements();              // your function to append new items
+//       observer.unobserve(entry.target); // stop observing the old one
+//       entry.target.classList.add('show')
+//     }
+//   });
+// }, {});
+
+const element = document.querySelectorAll("#section, .section2>.listOfcard>.cards")
 element.forEach(el => observe.observe(el));
